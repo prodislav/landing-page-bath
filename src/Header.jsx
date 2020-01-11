54b1f10700713a9549b7ed5687d7121077a16e83
Vladislav <hecobcemnpo@Gmail.com>
2020-01-11 11:57:42 +0300

    commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,27 +2,68 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 export const Header = React.memo(() => {
+
+    const [toggled, setToggled] = React.useState(true);
+
+    const toggleHover = () => {
+        // setToggled(!toggled)
+    }
     return (
-        <section className="border fix w-100 header_block">
-            <header className="header">
-                <div className="flex-sb m-1">
-                    <div className="header__logo">
-                        <Link to="/">Home</Link>
+        <>
+            {
+                toggled && <div className='menu' onMouseEnter={() => { }} onMouseLeave={() => { }}>
+                    <div className="flex-c">
+                        <div className="h-240 w-300 m-0-20 p-r">
+                            <Link to="/project/1">
+                                <div className="image i-13">
+                                    <div className="image-shadow"></div>
+                                    <div className='p-a image-text'>
+                                        Проекты домов
+                                    </div>
+                                    <div className='p-a image-cost'>
+                                        $$$
+                                </div>
+                                </div>
+
+                            </Link>
+                        </div>
+                        <div className="h-240 w-300 m-0-20 p-r">
+                            <div className="image i-12">
+                                <div className="image-shadow"></div>
+                                <div className='p-a image-text'>
+                                    Проекты бань
+                                </div>
+                                <div className='p-a image-cost'>
+                                    $$$
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <a href="tel:+78003331111" className="header__link">
-                        <span>8-800-333-11-11</span>
-                    </a>
                 </div>
-                <nav className="header__nav flex-sb">
-                    <Link to="/project/1"><div>Проекты домов</div></Link>
-                    <Link to="/bathes"><div>Проекты бань</div></Link>
-                    <div>Услуги</div>
-                    <div>Построенные дома</div>
-                    <div>Технологии</div>
-                    <div>О нас</div>
-                    <div>Контакты</div>
-                </nav>
-            </header>
-        </section >
+            }
+            <section className="fix w-100 header_block">
+                <header className="header">
+                    <div className="flex-sb align-i-c header--font">
+                        <Link to="/">
+                            <div className="header__logo">
+                            </div>
+                        </Link>
+                        <nav className="header__nav flex-sb">
+                            <div className='pr' onMouseEnter={toggleHover} onMouseLeave={toggleHover}>Проекты
+                        </div>
+                            <div>Услуги</div>
+                            <div>Галерея</div>
+                            <div>О нас</div>
+                            <div>Контакты</div>
+                        </nav>
+
+                        <a href="tel:+78003331111" className="header__link header--font">
+                            <span>8-800-333-11-11</span>
+                        </a>
+
+                    </div>
+                </header>
+            </section >
+        </>
     )
 })
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 export const Home = React.memo(() => {
     return (
@@ -24,15 +24,30 @@ export const Home = React.memo(() => {
                 <div className="block-wrapper">
                     <h1 className="main-section-title">Дома и бани по канадской технологии</h1>
                     <div className="flex-sb m-3">
-                        <div className="w-50 h-350">
-                            <div className="image i-12">
-                            <div className="image-shadow"></div>
-                            </div>
+                        <div className="w-50 h-350 p-r">
+                        <Link to="/project/1">
+                                <div className="image i-13">
+                                    <div className="image-shadow"></div>
+                                    <div className='p-a image-text'>
+                                        Проекты домов
+                                    </div>
+                                    <div className='p-a image-cost'>
+                                        $$$
+                                </div>
+                                </div>
+
+                            </Link>
                         </div>
-                        <div className="w-50 h-350">
+                        <div className="w-50 h-350 p-r">
                             <div className="image i-13">
                                 <div className="image-shadow"></div>
                             </div>
+                            <div className='p-a image-text'>
+                                Проекты бань
+                            </div>
+                            <div className='p-a image-cost'>
+                                $$$
+                                </div>
                         </div>
                     </div>
                 </div>
