bdb65c2dbdaf8a633bf3404a82813ff156e70996
Podolski Vlad <podolskivlad@outlook.com>
2020-01-13 23:01:14 +0300

    MVP-2.2

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,31 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const Item = React.memo(() => {
     const [toggled, setToggled] = React.useState(false);
-    const itemWrap = { width: '33%', height: '300px', display: 'flex', justifyContent: 'center', alignItems: 'center' }
+    const itemWrap = { width: '25%', height: '300px', display: 'flex', justifyContent: 'center', alignItems: 'center' }
     const itemContent = { width: '95%', height: '95%', padding: '2,5%', backgroundColor: '#f2f2f2', position: 'absolute' }
     return (
         <div style={itemWrap} className={`item-wrapper `} onMouseEnter={() => setToggled(true)} onMouseLeave={() => setToggled(false)}>
-            <div style={itemContent} className="item-content">
+            <Link to="/project/">
+                <div style={itemContent} className="item-content">
 
-                <div style={{ backgroundImage: 'url("./images/bath/bath-1-1.jpg")' }} className="item-image">
-                    <div className="hover-effect"></div>
-                </div>
-                <div style={{ margin: '0px' }} className="item-title t-align-c">
-                    <div className="hover-effect"></div>
-                    <div>Имя проекта</div>
-                    <div className="flex-sb">
-                        <span className="item-price">
-                            Цена
-            </span>
-                        <span className="item-area">
-                            Площадь
-            </span>
+                    <div style={{ backgroundImage: 'url("./images/bath/bath-1-1.jpg")' }} className="item-image">
+                        <div className="hover-effect"></div>
                     </div>
+                    <div style={{ margin: '0px' }} className="item-title t-align-c">
+                        <div className="hover-effect"></div>
+                        <div>Имя проекта</div>
+                        <div className="flex-sb">
+                            <span className="item-price">
+                                Цена
+            </span>
+                            <span className="item-area">
+                                Площадь
+            </span>
+                        </div>
+                    </div>
+
+
                 </div>
-
-
-            </div>
+            </Link>
         </div>
     )
 })
