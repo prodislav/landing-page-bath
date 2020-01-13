165ee056f77f50b2b51e0847abc9d5aa3e8b07e0
Podolski Vlad <podolskivlad@outlook.com>
2020-01-13 23:19:10 +0300

    MVP-2.6

--- src/Item.jsx
+++ src/Item.jsx
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 
 export const Item = React.memo(props => {
     console.log(props)
-    const itemWrap = { width: '25%', height: '300px', display: 'flex', justifyContent: 'center', alignItems: 'center' }
+    const itemWrap = { width: '33.33%', height: '300px', display: 'flex', justifyContent: 'center', alignItems: 'center' }
     const itemContent = { width: '95%', height: '95%', padding: '2,5%', backgroundColor: '#f2f2f2', position: 'absolute' }
     return (
         <div style={itemWrap} className={`item-wrapper `}>
             <Link to={`/project/${props.id}`}>
                 <div style={itemContent} className="item-content">
 
-                    <div style={{ backgroundImage: 'url("./images/bath/bath-1-1.jpg")' }} className="item-image">
+                    <div style={{ backgroundImage: `url("./images/bath/${props.defaultPhoto}")` }} className="item-image">
                         <div className="hover-effect"></div>
                     </div>
                     <div style={{ margin: '0px' }} className="item-title t-align-c">
                         <div className="hover-effect"></div>
-                        <div>Имя проекта</div>
+                        <div>{props.name}</div>
                         <div className="flex-sb">
                             <span className="item-price">
                                 Цена
