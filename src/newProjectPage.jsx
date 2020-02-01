d78edf0eaedc5401c363f79d9ebdb426dfa71fc6
Uladzislau Padolski <Uladzislau_Padolski@epam.com>
2020-02-01 14:09:28 +0300

    fix

--- src/newProjectPage.jsx
+++ src/newProjectPage.jsx
@@ -68,35 +68,10 @@ export const NewProjectPage = React.memo((props) => {
                                 </div>
                                 <span className="f-s-24">терраса</span>
                             </div>
-
-                            {/* <div className='flex-sb'>
-                                <div className="text-c">Площадь террасы:</div>
-                                <div className="text-b">26 м2</div>
-                            </div> */}
                         </div>
                     </div>
 
                     <div className='line'></div>
-
-                    {/* <div className="m-30">
-
-                            <div className='text-b-2'>
-                                Комплект дома:
-                            </div>
-
-                            <div className='text-b-3'>
-                                Состоит из:
-                            </div>
-                            <ul className="project-d-list">
-                                <li>Набор раскроенных панелей</li>
-                                <li>Пиломатериал сухой строганый</li>
-                                <li>Клееный брус</li>
-                                <li>Крепеж (41 мм, 75 мм, 120 мм.)</li>
-                                <li>Специализированный силовой крепеж (180мм - 360мм)</li>
-                                <li>Материалы, улучшающие теплоизоляцию и герметичность дома</li>
-                            </ul>
-                                </div> */}
-
                     <Tabs className="w-100">
                         <TabList>
                             <Tab>Планировка</Tab>
